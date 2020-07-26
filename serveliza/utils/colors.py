826bc640f8c835e0a801e76cc1b40287aa755077
chivke <chivke@pm.me>
2020-07-25 21:36:59 -0400

    Add basic lambda functions for colorize prints

---
+++ serveliza/utils/colors.py
@@ -0,0 +1,7 @@
+
+# Utils to give color to console print.
+
+INFO = lambda x : '\033[94m'+str(x)+'\033[0m'
+OK = lambda x: '\033[0;32m'+str(x)+'\033[0m'
+WARN = lambda x: '\033[0;33m'+str(x)+'\033[0m'
+LEAD = lambda x: '\033[90m'+str(x)+'\033[0m'
