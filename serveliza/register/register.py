a5e0fae15f684245986e621b45f783b38979350d
chivke <chivke@pm.me>
2020-07-30 13:51:04 -0400

    Refact register tool main class

--- serveliza/register/register.py
+++ serveliza/register/register.py
@@ -4,71 +4,49 @@ from tqdm import tqdm
 from ..mixins.pdf import DirFilePDFMixin
 from ..utils import colors
 from .parsers import SheetRegisterParser
+from . import printers
+from . import serializers
 
 
 class ElectoralRegister(DirFilePDFMixin):
     '''
-    Class to process electoral register \
-    file (padrón electoral) of chilean electoral \
-    service (Servicio Electoral de Chile, Servel) \
-    in PDF format.
+    :class:`ElectoralRegister <.ElectoralRegister>` allows to \
+    instantiate an electoral register of the chilean Electoral Service \
+    (Servicio Electoral de Chile, SERVEL) from PDF files.
 
-    The constructor of instance require specify a \
-    \'source\' param with the path string of directory \
-    or PDF file to process. The constructor is heredit \
-    of <.DirFilePDFMixin>.
+    The path of the directory or file to be used as the data source must \
+    be specified through the "source" parameter when instantiating the \
+    object. The "verbose" parameter determines if the progress and the \
+    result will be printed on the screen or will be kept silent, it accepts \
+    only Boolean values. Both characteristics are inherited from \
+    :class:`DirFilePDFMixin <.DirFilePDFMixin>` class.
 
-    Testing with electoral register of 2013 (presidential, \
-    senatorial, parliamentary and regional councilors) and \
-    2016 (municipal) elections. *16/07/20*
+    The :meth:`run <.ElectoralRegister.run>` method starts the analysis \
+    process of the text extracted from each sheet of the files. A parser \
+    (:class:`SheetRegisterParser <.SheetRegisterParser>`) will be iterated \
+    over each sheet, which will be instantiated from the class defined in \
+    the class attribute :attr:`parser_class <.ElectoralRegister.parser_class` \
+    and called through the :meth:`parse_sheet <.ElectoralRegister.parse_sheet>`
+    method. Each resulting object will be sent to the :meth:`serialize_sheet \
+    <.ElectoralRegister.serialize_sheet>` method that will store the result \
+    in the :attr:`storage <.ElectoralRegister.storage>` property, and then \
+    generate a partial export ( with the :meth:`export_sheet \
+    <.ElectoralRegister.export_sheet` method) if so specified.
+
+    Setting the parameter "auto" to true in the constructor will \
+    automatically start the :meth:`run <.ElectoralRegister.run>` method.
+
+    >>> obj = ElectoralRegister(
+        source='/path/to/the/pdf/file(s)',
+        verbose=True,
+        auto=True)
+
     '''
-    parser_class    = SheetRegisterParser
-    
+    parser_class = SheetRegisterParser
+
     @property
     def resume(self):
         return print(self)
-
-    def __str__(self):
-        resume = f'{self.__class__.__name__} instance'
-        if self.runned:
-            resume += colors.OK(' [runned]')
-            resume += colors.LEAD(f' {str(len(self.metadata["files"]))} file')
-            if len(self.metadata['files']) > 1:
-                resume += 's'
-        else:
-            resume += colors.WARN(' [not runned]')
-        resume += colors.LEAD('\n'+('-'*len(resume)))
-        storages = len(self.storage)
-        if storages > 1:
-            resume += colors.WARN(f'\n[warn]: {str(storages)}'\
-                +' different electoral register loaded. Dont do it,'\
-                +' there may be repetitions of people.')
-        screen_width = os.get_terminal_size().columns
-        for key,bucket in self.storage.items():
-            column_width = screen_width // len(bucket['fields'])
-            total_entries = bucket["metadata"]["entries-total"]
-            resume += '\n['+colors.OK(key)\
-                +f']: data of {colors.INFO(total_entries)} people'
-            errors = (100 / total_entries)*len(self.errors)
-            resume += colors.LEAD(f' [{errors:.5} % of errors]')
-            divider = colors.LEAD('|')
-            divider_width = colors.LEAD('\n'+('-'*screen_width))
-            cutter = lambda x: '.' if len(x) - (column_width -4) > 0 else (' ')
-            column_aux = colors.LEAD(divider+' {:<'+str(column_width -3 )+'} ')
-            fields = '\n' + str(column_aux*len(bucket['fields'])).format(
-                *[colors.INFO(x[:column_width -4])+cutter(x) for x in bucket['fields']]) + divider
-            resume += divider_width + fields + divider_width
-            resume += '\n' + str(column_aux*len(bucket['fields'])).format(
-                *[x[:column_width -4]+cutter(x) for x in bucket['entries'][0]]) + divider
-            resume += divider_width
-            total_msg = colors.OK(f'[ {str(total_entries - 2)} entries ]')
-            separator = ( screen_width - len(total_msg) ) // 2
-            resume += '\n'+colors.LEAD('-'*separator)+total_msg+colors.LEAD('-'*separator)
-            resume += divider_width
-            resume += '\n' + str(column_aux*len(bucket['fields'])).format(
-                *[x[:column_width -4]+cutter(x) for x in bucket['entries'][-1]]) + divider
-            resume += divider_width
-        return resume
 
     @property
     def runned(self):
@@ -110,25 +88,35 @@ class ElectoralRegister(DirFilePDFMixin):
 
     def run(self):
         starting = datetime.now()
-        self.metadata['analysis'] = {'starting': starting}
+        self._metadata['analysis'] = {'starting': starting}
         sheets = self.sheets
         if self.verbose:
             total_sheets = colors.INFO(len(sheets))
             print(f'Initiating analysis of {total_sheets} sheets')
             print(colors.LEAD(f"At {str(starting)}"))
             sheets = tqdm(sheets)
-        for idx,sheet in enumerate(sheets):
+        for idx, sheet in enumerate(sheets):
             if self.verbose:
                 sheets.set_description(
                     f'> {str(idx)}/{total_sheets} sheets')
             parsed = self.parse_sheet(sheet)
-            parsed = self.storage_sheet(parsed)
+            parsed = self.serialize_sheet(parsed)
         self._runned = True
         self.storage_consolidate()
-        self.resume
+        #self.resume
 
     def parse_sheet(self, sheet):
         return self.parser_class(sheet=sheet)
+
+    def serialize_sheet(self, parsed):
+        rid = serializers.storage_rid(self, parsed)
+        payload = self, parsed, rid
+        serializers.metadata_times(*payload)
+        serializers.metadata_place(*payload)
+        serializers.metadata_entries(*payload)
+        serializers.metadata_nulls(*payload)
+        self._storage[rid]['entries'] += parsed.entries
+        self._errors += parsed.errors
 
     def storage_sheet(self, parsed):
         # ..
@@ -210,7 +198,13 @@ class ElectoralRegister(DirFilePDFMixin):
         self._storage = {}
         self._errors = []
         self._runned = False
-        if auto: 
+        if auto:
             if self.verbose:
                 print(colors.INFO('Automatic initialization'))
             self.run()
+
+    def __str__(self):
+        return printers.resume_electoral(self)
+
+    def __repr__(self):
+        return printers.repr_electoral(self)
