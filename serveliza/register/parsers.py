bd883e7e4ff85138771fda6461aa5d34c9bd8d76
chivke <chivke@pm.me>
2020-07-30 13:50:33 -0400

    Refact parser from electoral register

--- serveliza/register/parsers.py
+++ serveliza/register/parsers.py
@@ -13,6 +13,7 @@ class SheetRegisterParser:
     Sheet to parse must be provied in construtor \
     and be string.
     '''
+
     regex_begin = r'^REPUBLICA\s+DE\s+CHILE'
     regex_register = r'(PADRON\s+ELECTORAL\s+[A-Z]+)\s+-?\s+ELECCIONES'
     regex_election = r'ELECCIONES[A-Z,\s]+\d+'
@@ -20,10 +21,11 @@ class SheetRegisterParser:
     regex_commune = r'COMUNA[0,]*\s*:\s*([A-Z\s]*\s{3})'
     regex_province = r'PROVINCIA[0,]*\s*:\s*([A-Z\s]*)'
 
-    regex_name  = r'^[A-ZÑa-z\s]+' 
-    regex_rut   = r'\d*\.?\d+\.\d+-[0-9kK]'
-    regex_sex   = r'\sVAR\s|\sMUJ\s'
-    regex_table = r'\s(\d+\s?\w?)\s*\d*$'
+    regexs_entries = {
+        'name': r'^[A-ZÑa-z\s]+',
+        'rut': r'\d*\.?\d+\.\d+-[0-9kK]',
+        'sex': r'\sVAR\s|\sMUJ\s',
+        'table': r'\s(\d+\s?\w?)\s*\d*$'}
 
     dpa_fixture_path = 'serveliza/utils/fixtures/'\
         'DPA-commune-circuns.json'
@@ -78,101 +80,78 @@ class SheetRegisterParser:
             self._decomposed = True
 
     def run(self):
-        start_at   = dt.now()
+        start_at = dt.now()
         self.decompose()
-        header_at  = dt.now()
+        header_at = dt.now()
         self.parse_header()
-        fields_at  = dt.now()
+        fields_at = dt.now()
         self.parse_fields()
         entries_at = dt.now()
         self.parse_entries()
         finish_at = dt.now()
-        times = {}
-        times['total'] = finish_at - start_at
-        times['parse-header'] = fields_at - header_at
-        times['parse-fields'] = entries_at - fields_at
-        times['parse-entries'] = finish_at - entries_at
-        if not 'times' in self._metadata:
+        times = {
+            'total': finish_at - start_at,
+            'header': fields_at - header_at,
+            'fields': entries_at - fields_at,
+            'entries': finish_at - entries_at}
+        if 'times' not in self._metadata:
             self._metadata['times'] = {}
         self._metadata['times'].update(times)
 
-    def parser(self, regex, target, ecode):
-        parsed = re.findall(regex, target)
-        if not bool(parsed):
-            #import pdb; pdb.set_trace()
-            self._errors.append( {
-                'code'  : ecode,
-                'regex' : regex,
-                'target': target } )
-            return None
-        return parsed[0].strip()
-
     def parse_header(self):
-        '''Return header data.'''
-        header = {}
-        # validate begin of sheet
-        self.parser(self.regex_begin, self.sheet[0],
+        '''
+        Return header data.
+        '''
+        def __identify(header):
+            id_reg = ''.join([w[0] for w in header['register'].split(' ')])
+            id_ele = re.sub(r'[0-9Y]', '', ''.join(
+                [w[0] for w in header['election'].split(' ')]))
+            return f'{id_reg}-{id_ele}-{str(header["year"])}'
+        self.__parser(
+            self.regex_begin, self.sheet[0],
             'header-invalid-begin')
-        # type of electoral register
         attributes = [
             ('register', self.sheet[0]),
             ('election', self.sheet[0]),
             ('region',   self.sheet[1]+self.sheet[2]),
             ('commune',  self.sheet[1]+self.sheet[2]),
             ('province', self.sheet[2]+'--'+self.sheet[3])]
-        for key,target in attributes:
-            header[key] = self.parser(
-                regex=getattr(self,'regex_'+key),
-                target=target, ecode='header-no-'+key )
-        header['year'] = int( self.parser(r'\d+$', 
-            header['election'], 'header-no-year') )
-        id_reg = ''.join( [w[0] for w in header['register']\
-            .split(' ') ])
-        id_ele = re.sub(r'[0-9Y]','',''.join(
-            [w[0] for w in header['election'].split(' ') ]) )
-        identify = f'{id_reg}-{id_ele}-{str(header["year"])}'
-        # load header to de property
+        header = {}
+        for key, target in attributes:
+            header[key] = self.__parser(
+                regex=getattr(self, 'regex_'+key),
+                target=target, ecode='header-no-'+key)
+        header['year'] = int(self.__parser(
+            r'\d+$', header['election'], 'header-no-year'))
+        self._metadata['rid'] = __identify(header)
         self._header = header
-        self._metadata['rid'] = identify
-        # load valid circunscriptions
         with open(self.dpa_fixture_path) as f:
             fixture = json.load(f)
         if header['commune'] in fixture:
             self._circuns = fixture[header['commune']]
         else:
-            self._errors.append( {
-            'code'  : 'commune-not-in-fixture',
-            'fixture': self.dpa_fixture_path,
-            'target': header['commune'] } )
-
-    def get_fields_index(self):
-        if self.fields_index:
-            return self.fields_index
-        regex = r'^NOMBRE\s+C'
-        for idx,line in enumerate(self.sheet):
-            if re.match(regex, line):
-                self._fields_index = idx
-                return idx
-        self._errors.append( {
-            'code'  : 'fields-no-index',
-            'regex' : regex,
-            'target': self.sheet } )
+            self._circuns = None
+            self._errors.append({
+                'code': 'commune-not-in-fixture',
+                'fixture': self.dpa_fixture_path,
+                'target': header['commune']})
 
     def parse_fields(self):
-        index = self.get_fields_index()
+        index = self.__get_fields_index()
         if not index:
             return None
         fields_line = self.sheet[index]
         fields_line = fields_line.replace(
             'LIO ELE', 'LIO-ELE')
-        self._fields = list(map(lambda x: slugify(x), 
-            fields_line.split()) )
+        self._fields = list(map(
+            lambda x: slugify(x),
+            fields_line.split()))
         self._fields.insert(3, 'comuna')
         self._fields.insert(3, 'provincia')
         self._fields.insert(3, 'region')
 
     def parse_entries(self):
-        index = self.get_fields_index() + 1
+        index = self.__get_fields_index() + 1
         if not index:
             return None
         entries = []
@@ -185,95 +164,202 @@ class SheetRegisterParser:
             entry = self.parse_entry(line)
             if entry:
                 entries.append(entry)
-            else:
-                import pdb; pdb.set_trace()
-        #self._entries = entries
         total_entries = len(entries)
-        self._metadata['entries-total'] = total_entries
+        self.metadata['entries'] = {}
         if malformed:
             # try to take entry again
-            pentries = []
-            lastentry = ''
-            for txt in malformed:
-                if re.match(r'^\w+', txt):
-                    if lastentry:
-                        pentries.append(lastentry)
-                    lastentry = txt
-                elif lastentry:
-                    lastentry += txt
-                else:
-                    self._errors.append({
-                        'code'  : 'malformed-no-start',
-                        'target': txt } )
-            for e in pentries:
-                entry = self.parse_entry(e)
-                if not entry:
-                    self._errors.append({
-                        'code'  : 'malformed-no-entry',
-                        'target': e } )
-                else:
-                    entries.append(entry)
-            self._metadata['entries-rescue'] = len(entries) - total_entries
-            self._metadata['entries-total'] += self._metadata['entries-rescue']
+            rescue_entries = self.__rescue_entries(malformed)
+            entries += rescue_entries
+            #pentries = []
+            #lastentry = ''
+            #for txt in malformed:
+            #    if re.match(r'^\w+', txt):
+            #        if lastentry:
+            #            pentries.append(lastentry)
+            #        lastentry = txt
+            #    elif lastentry:
+            #        lastentry += txt
+            #    else:
+            #        self._errors.append({
+            #            'code'  : 'malformed-no-start',
+            #            'target': txt } )
+            #for e in pentries:
+            #    entry = self.parse_entry(e)
+            #    if not entry:
+            #        self._errors.append({
+            #            'code'  : 'malformed-no-entry',
+            #            'target': e } )
+            #    else:
+            #        entries.append(entry)
+            self._metadata['entries']['rescue'] = len(entries) - total_entries
+            total_entries = len(entries)
+        self._metadata['entries']['total'] = total_entries
         self._entries = entries
 
     def parse_entry(self, line):
-        entry = []
-        first_fields = ['name','rut','sex','circun','table']
-        dir_ini = None
-        dir_end = None
-        for key in first_fields:
+        def __regex_fields(line, fields):
+            attr_fields = ['name', 'rut', 'sex', 'table']
+            fields = [fields[0], fields[1], fields[2], fields[-1]]
+            values = []
+            for idx, field in enumerate(fields):
+                value = self.__parser(
+                    regex=self.regexs_entries[attr_fields[idx]],
+                    target=line, ecode='entry-'+field)
+                values.append(value)
+                if not value:
+                    if field not in self._metadata['nulls']:
+                        self._metadata['nulls'][field] = 1
+                    else:
+                        self._metadata['nulls'][field] += 1
+            return values
+
+        def __parse_circun(line, field):
             value = None
-            if key == 'circun':
-                for cir in self.circuns:
-                    if cir in line:
-                        value = cir
-            else:
-                value = self.parser(
-                    regex=getattr(self, 'regex_'+key),
-                    target=line, ecode='entry-'+key)
-            if value:
-                if key == 'sex':
-                    dir_ini = re.search(value, line).end()
-                elif key == 'circun':
-                    dir_end = re.search(value, line).start()
-            else:
-                self._metadata['nulls-total'] += 1
-                if not 'nulls-'+key in self._metadata:
-                    self._metadata['nulls-'+key] = 1
+            for cir in self.circuns:
+                if cir in line:
+                    value = cir
+            if not value:
+                self._errors.append({
+                    'code': 'entry-' + field + '-not-found',
+                    'circuns': self.circuns,
+                    'target': line})
+                if field not in self._metadata['nulls']:
+                    self._metadata['nulls'][field] = 1
                 else:
-                    self._metadata['nulls-'+key] += 1
-            entry.append(value)
-        # electoral direction
-        direction = None
-        if dir_ini and dir_end:
-            direction = line[dir_ini:dir_end].strip()
-        else:
-            self._metadata['nulls-total'] += 1
-            if not 'nulls-dir' in self._metadata:
-                self._metadata['nulls-dir'] = 1
+                    self._metadata['nulls'][field] += 1
+            return value
+
+        def __parse_dir(line, sex, circun, field):
+            ini = re.search(sex, line).end()
+            end = re.search(circun, line).start()
+            if ini and end:
+                return line[ini:end].strip()
             else:
-                self._metadata['nulls-dir'] += 1
+                self._errors.append({
+                    'code': 'entry-' + field + '-not-found',
+                    'reason': 'sex end and circun init not found',
+                    'target': line})
+                if field not in self._metadata['nulls']:
+                    self._metadata['nulls'][field] = 1
+                else:
+                    self._metadata['nulls'][field] += 1
+
+        if 'nulls' not in self.metadata:
+            self._metadata['nulls'] = {'total': 0}
+        regex_vals = __regex_fields(line, self.fields)
+        entry = regex_vals
+        circun = __parse_circun(line, self.fields[-2])
+        entry.insert(3, circun)
+        direction = __parse_dir(line, regex_vals[2], circun, self.fields[-3])
         entry.insert(3, direction)
         entry.insert(3, self.header['commune'])
         entry.insert(3, self.header['province'])
         entry.insert(3, self.header['region'])
         return entry
 
-    def __init__(self, sheet, auto=True, *args, **kwargs):
-        if not isinstance(sheet, str):
-            raise TypeError('\'sheet\' arg must be string')
+        # entry = []
+        # first_fields = ['name','rut','sex','circun','table']
+        # dir_ini = None
+        # dir_end = None
+        # for key in first_fields:
+        #     value = None
+        #     if key == 'circun':
+        #         for cir in self.circuns:
+        #             if cir in line:
+        #                 value = cir
+        #     else:
+        #         value = self.__parser(
+        #             regex=getattr(self, 'regex_'+key),
+        #             target=line, ecode='entry-'+key)
+        #     if value:
+        #         if key == 'sex':
+        #             dir_ini = re.search(value, line).end()
+        #         elif key == 'circun':
+        #             dir_end = re.search(value, line).start()
+        #     else:
+        #         self._metadata['nulls-total'] += 1
+        #         if not 'nulls-'+key in self._metadata:
+        #             self._metadata['nulls-'+key] = 1
+        #         else:
+        #             self._metadata['nulls-'+key] += 1
+        #     entry.append(value)
+        # # electoral direction
+        # direction = None
+        # if dir_ini and dir_end:
+        #     direction = line[dir_ini:dir_end].strip()
+        # else:
+        #     self._metadata['nulls-total'] += 1
+        #     if 'nulls-dir' not in self._metadata:
+        #         self._metadata['nulls-dir'] = 1
+        #     else:
+        #         self._metadata['nulls-dir'] += 1
+        # entry.insert(3, direction)
+        # entry.insert(3, self.header['commune'])
+        # entry.insert(3, self.header['province'])
+        # entry.insert(3, self.header['region'])
+        # return entry
+
+    def __rescue_entries(self, malformed):
+        pentries = []
+        entries = []
+        lastentry = ''
+        for txt in malformed:
+            if re.match(r'^\w+', txt):
+                if lastentry:
+                    pentries.append(lastentry)
+                lastentry = txt
+            elif lastentry:
+                lastentry += txt
+            else:
+                self._errors.append({
+                    'code': 'malformed-no-start',
+                    'target': txt})
+        for e in pentries:
+            entry = self.parse_entry(e)
+            if not entry:
+                self._errors.append({
+                    'code': 'malformed-no-entry',
+                    'target': e})
+            else:
+                entries.append(entry)
+        return entries
+
+    def __get_fields_index(self):
+        if self.fields_index:
+            return self.fields_index
+        regex = r'^NOMBRE\s+C'
+        for idx, line in enumerate(self.sheet):
+            if re.match(regex, line):
+                self._fields_index = idx
+                return idx
+        self._errors.append({
+            'code': 'fields-no-index',
+            'regex': regex,
+            'target': self.sheet})
+
+    def __parser(self, regex, target, ecode):
+        parsed = re.findall(regex, target)
+        if not bool(parsed):
+            self._errors.append({
+                'code': ecode,
+                'regex': regex,
+                'target': target})
+            return None
+        return parsed[0].strip()
+
+    def __launch_props(self):
         self._decomposed = False
-        self._metadata = { 
-            'nulls-total' :  0,
-            'entries-total': 0,
-            'entries-rescue':0,}
-        self._sheet = sheet
+        self._fields_index = None
+        self._metadata = {}
         self._header = {}
         self._fields = []
         self._entries = []
         self._errors = []
-        self._fields_index = None
-        self._circuns_of = None
+
+    def __init__(self, sheet, auto=True, *args, **kwargs):
+        if not isinstance(sheet, str):
+            raise TypeError('\'sheet\' arg must be string')
+        self.__launch_props()
+        self._sheet = sheet
         if auto:
             self.run()
