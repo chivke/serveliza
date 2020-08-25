6682810f0a545a92839d5c7e02196489c8713b2b
chivke <chivke@pm.me>
2020-08-25 09:45:29 -0400

    Itegrate requirements to setup.

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+with open('requirements.txt') as req_file:
+    requirements = req_file.read()
 
 setup_requirements = [ ]
 
@@ -19,15 +20,13 @@ test_requirements = [ ]
 setup(
     author="chivke",
     author_email='chivke@pm.me',
-    python_requires='>=3.5',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
@@ -44,9 +43,9 @@ setup(
     keywords='serveliza',
     name='serveliza',
     packages=find_packages(include=['serveliza', 'serveliza.*']),
-    setup_requires=setup_requirements,
+    # setup_requires=setup_requirements,
     test_suite='tests',
-    tests_require=test_requirements,
+    # tests_require=test_requirements,
     url='https://github.com/chivke/serveliza',
     version='0.1.0',
     zip_safe=False,
